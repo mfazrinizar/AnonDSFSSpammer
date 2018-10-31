6478060e1b280ff1f71245fd4d1074bcb5658aac
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-10-31 16:02:03 +0700

    Main File

---
+++ AnonDSFSSpammer.sh
@@ -0,0 +1,160 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Anon DSFS Spammer" | lolcat
+echo "\033[35;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mCreator : Anon6372098" 
+echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R"
+echo "\033[36;1mThanks to : Tuan c4rt00nw4r"
+echo "\033[37;1mAlly Team : DSFS Public"
+echo "\033[33;1mContact Me : anon6372098@gmail.com"
+echo "\033[31;1mSubscribe Youtube :"
+echo "\033[33;1mD4RK SYST3M F41LUR3 S33K3R" "\033[32;1m&" "\033[36;1m(DSFS)"
+echo "\033[34;1m[============================]"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tool Spam :"
+echo "\033[36;1m"
+echo "[===============================================]"
+echo "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo "2.> Spam Sms Telkomsel"
+echo "[===============================================]>"
+echo "3.> Spam Sms Matahari Mall"
+echo "[===============================================]>"
+echo "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo "6.> Spam Bom PHD"
+echo "[===============================================]>"
+echo "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo "9.> Spam Bom JDID"
+echo "[===============================================]>"
+echo "10.> Spam Grab"
+echo "[===============================================]>"
+echo "11.> Spam BukaLapak WhatsApp"
+echo "[===============================================]>"
+read -p "root@Tools-AnonDSFSSpammer]~#" anon
+
+if [ $anon = 1 ] || [ $anon = 1 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php tp.php
+fi
+
+if
+[ $anon = 2 ] || [ $anon = 2 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php t.php
+fi
+
+if [ $anon = 3 ] || [ $anon = 3 ]
+then
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php hooq.php
+fi
+
+if [ $anon = 5 ] || [ $anon = 5 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php kfc.php
+fi
+
+if [ $anon = 6 ] || [ $anon = 6 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php phd.php
+fi
+
+if [ $anon = 7 ] || [ $anon = 7 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php whiskas.php
+fi
+
+if [ $anon = 8 ] || [ $anon = 8 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php zipay.php
+fi
+
+if [ $anon = 9 ] || [ $anon = 9 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php jdid.php
+fi
+
+if [ $anon = 10 ] || [ $anon = 10 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+python2 spammer.py
+fi
+
+if [ $anon = 10 ] || [ $anon = 10 ]
+then
+clear
+figlet "Anon DSFS Spammer" | lolcat
+cd module
+php DSFS-WA-Spam.php
+fi
+
+if [ $anon = 0 ] || [ $anon = 00 ]
+then
+echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+sleep 1
+echo "\033[32;1mWe Are DSFS Public"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not Forgive"
+sleep 1
+echo "We Do Not Forget"
+sleep 1
+echo "Expect Us"
+sleep 1
+echo "\033[32;1mDiakui, bukan mengakui"
+sleep 1
+exit
+fi
