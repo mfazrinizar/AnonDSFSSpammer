dea563ccbfcdb3b566f37c4c80f05b1defc09d41
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-11-01 14:04:27 +0700

    File

---
+++ module/run.php
@@ -0,0 +1,21 @@
+﻿<?php
+include 'func.php';
+
+/*
+    https://github.com/Anon6372098 
+    Github : Anon6372098
+*/
+
+$init = new Bom();
+
+//Eksekusi Call/Sms Boomber (Limit 3x/Jam)
+$init->type = 2; //Ketik 2 untuk telpon, Ketik 1 untuk sms
+$init->no = ""; //Nomor Hp tujuan
+
+if ($init->type == 1) {
+	for ($i=0; $i < 2; $i++) { 
+	    $init->Verif($init->no,$init->type);
+	}
+}elseif ($init->type == 2) {
+	 $init->Verif($init->no,$init->type);
+}
--- module/spammer.py
+++ module/spammer.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 
-# - Spammer v2
+# - Spammer v3.0
 # | Description: spams a phone number by sending it a lot of sms by using Grab API
-# | Author: P4kL0nc4t
-# | Date: 5/12/2017
-# | Disclaimer: Editing author will not make you the real coder
+# | Spam: Grab
+# | Date: 01/11/2018
+# | Disclaimer: For Fun Purpose Only
+# | What's New?
+# | - Added coloring
+# | - Added support multiple proxies (BETA)
+# | - Added feature to create a new separate thread for each proxy in order to increase spam speed (BETA)
+# | - Added support for phone number with +xxxxxx format 
+# | - And another fixes
 
 import spammer_class
 spammer = spammer_class.Spammer()
-spammer.author = "P4kL0nc4t"
-spammer.main()
+spammer.tema = "Grab Spam"
+try:
+    spammer.main()
+except KeyboardInterrupt:
+    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
+    exit()
+
